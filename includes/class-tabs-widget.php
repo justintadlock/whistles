1297d45b391b5571732c8a60053936e53c667b2b
Justin Tadlock <justin@justintadlock.com>
2013-08-22 15:19:43 -0500

    Working tabs widget.

--- includes/class-tabs-widget.php
+++ includes/class-tabs-widget.php
@@ -1,107 +1,193 @@
 <?php
-/**
- * File for registering custom post types.
- *
- * @package    TabsAndJazz
- * @subpackage Includes
- * @since      0.1.0
- * @author     Justin Tadlock <justin@justintadlock.com>
- * @copyright  Copyright (c) 2013, Justin Tadlock
- * @link       http://themehybrid.com/plugins/tabs-and-jazz
- * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- */
-
-/* Register custom post types on the 'init' hook. */
-add_action( 'init', 'taj_register_post_types' );
 
 /**
- * Registers post types needed by the plugin.
+ * Tabs widget class
  *
- * @since  0.1.0
- * @access public
- * @return void
+ * @since 0.1.0
  */
-function taj_register_post_types() {
+class TAJ_Widget_Tabs extends WP_Widget {
 
-	/* Set up the arguments for the portfolio item post type. */
-	$args = array(
-		'description'         => '',
-		'public'              => true,
-		'publicly_queryable'  => false,
-		'show_in_nav_menus'   => false,
-		'show_in_admin_bar'   => true,
-		'exclude_from_search' => true,
-		'show_ui'             => true,
-		'show_in_menu'        => false,
-	//	'menu_position'       => 15,
-	//	'menu_icon'           => CCP_URI . 'images/menu-icon.png',
-		'can_export'          => true,
-		'delete_with_user'    => false,
-		'hierarchical'        => false,
-		'has_archive'         => false,
-		'query_var'           => 'tab',
-		'capability_type'     => 'tab',
-		'map_meta_cap'        => true,
+	/**
+	 * Set up the widget's unique name, ID, class, description, and other options.
+	 *
+	 * @since 1.2.0
+	 */
+	function __construct() {
 
-		/* Only 3 caps are needed: 'manage_tabs', 'create_tabs', and 'edit_tabs'. */
-		'capabilities' => array(
+		/* Set up the widget options. */
+		$widget_options = array(
+			'classname'   => 'taj',
+			'description' => esc_html__( 'Bells and whistles.', 'tabs-and-jazz' )
+		);
 
-			// meta caps (don't assign these to roles)
-			'edit_post'              => 'edit_tab',
-			'read_post'              => 'read_tab',
-			'delete_post'            => 'delete_tab',
+		/* Set up the widget control options. */
+		$control_options = array(
+			'width'  => 200,
+			'height' => 350
+		);
 
-			// primitive/meta caps
-			'create_posts'           => 'create_tabs',
+		/* Create the widget. */
+		$this->WP_Widget(
+			'taj',                         // $this->id_base
+			__( 'Tabs', 'tabs-and-jazz' ), // $this->name
+			$widget_options,               // $this->widget_options
+			$control_options               // $this->control_options
+		);
+	}
 
-			// primitive caps used outside of map_meta_cap()
-			'edit_posts'             => 'edit_tabs',
-			'edit_others_posts'      => 'manage_tabs',
-			'publish_posts'          => 'manage_tabs',
-			'read_private_posts'     => 'read',
+	/**
+	 * Outputs the widget based on the arguments input through the widget controls.
+	 *
+	 * @since 0.1.0
+	 */
+	function widget( $sidebar, $instance ) {
+		extract( $sidebar );
 
-			// primitive caps used inside of map_meta_cap()
-			'read'                   => 'read',
-			'delete_posts'           => 'manage_tabs',
-			'delete_private_posts'   => 'manage_tabs',
-			'delete_published_posts' => 'manage_tabs',
-			'delete_others_posts'    => 'manage_tabs',
-			'edit_private_posts'     => 'edit_tabs',
-			'edit_published_posts'   => 'edit_tabs'
-		),
+		/* Set the $args for wp_get_archives() to the $instance array. */
+		$args = $instance;
 
-		/* The rewrite handles the URL structure. */
-		'rewrite' => false,
+		/* Overwrite the $echo argument and set it to false. */
+		$args['echo'] = false;
 
-		/* What features the post type supports. */
-		'supports' => array(
-			'title',
-			'editor',
-		//	'excerpt',
-		//	'author',
-		//	'thumbnail'
-		),
+		/* Output the theme's $before_widget wrapper. */
+		echo $before_widget;
 
-		/* Labels used when displaying the posts. */
-		'labels' => array(
-			'name'               => __( 'Tabs',                   'tabs-and-jazz' ),
-			'singular_name'      => __( 'Tab',                    'tabs-and-jazz' ),
-			'menu_name'          => __( 'Tabs',                   'tabs-and-jazz' ),
-			'name_admin_bar'     => __( 'Tabs',                   'tabs-and-jazz' ),
-			'add_new'            => __( 'Add New',                'tabs-and-jazz' ),
-			'add_new_item'       => __( 'Add New Tab',            'tabs-and-jazz' ),
-			'edit_item'          => __( 'Edit Tab',               'tabs-and-jazz' ),
-			'new_item'           => __( 'New Tab',                'tabs-and-jazz' ),
-			'view_item'          => __( 'View Tab',               'tabs-and-jazz' ),
-			'search_items'       => __( 'Search Tabs',            'tabs-and-jazz' ),
-			'not_found'          => __( 'No tabs found',          'tabs-and-jazz' ),
-			'not_found_in_trash' => __( 'No tabs found in trash', 'tabs-and-jazz' ),
-			'all_items'          => __( 'Tabs',                   'tabs-and-jazz' ),
-		)
-	);
+		/* Arguments for the tab set. */
+		$args = array(
+			'group'   => $instance['group'],
+			'limit'   => $instance['limit'],
+			'type'    => $instance['type'],
+			'order'   => $instance['order'],
+			'orderby' => $instance['orderby']
+		);
 
-	/* Register the portfolio item post type. */
-	register_post_type( 'tab', $args );
+		/* If a title was input by the user, display it. */
+		if ( !empty( $instance['title'] ) )
+			echo $before_title . apply_filters( 'widget_title',  $instance['title'], $instance, $this->id_base ) . $after_title;
+
+
+		$tabs = new Tabs_Bells_Whistles( $args );
+		echo $tabs->get_tabs();
+
+		/* Close the theme's widget wrapper. */
+		echo $after_widget;
+	}
+
+	/**
+	 * Updates the widget control options for the particular instance of the widget.
+	 *
+	 * @since 0.1.0
+	 */
+	function update( $new_instance, $old_instance ) {
+
+		$instance = $new_instance;
+
+		$instance['title']   = strip_tags( $new_instance['title'] );
+		$instance['group']   = strip_tags( $new_instance['group'] );
+		$instance['type']    = strip_tags( $new_instance['type'] );
+		$instance['order']   = strip_tags( $new_instance['order'] );
+		$instance['orderby'] = strip_tags( $new_instance['orderby'] );
+
+		$instance['limit'] = ( 0 >= $new_instance['limit'] ) ? -1 : absint( $new_instance['limit'] );
+
+		return $instance;
+	}
+
+	/**
+	 * Displays the widget control options in the Widgets admin screen.
+	 *
+	 * @since 0.1.0
+	 */
+	function form( $instance ) {
+
+		$terms = get_terms( 'tab_group' );
+
+		if ( !empty( $terms ) && !is_wp_error( $terms ) ) {
+			$all_terms = $terms;
+			$default_term = array_shift( $all_terms );
+			$default_term = $default_term->slug;
+		} else {
+			$default_term = '';
+		}
+
+		/* Set up the default form values. */
+		$defaults = array(
+			'title'   => '',
+			'group'   => $default_term,
+			'limit'   => -1,
+			'type'    => 'tab',
+			'order'   => 'DESC',
+			'orderby' => 'date',
+		);
+
+		/* Merge the user-selected arguments with the defaults. */
+		$instance = wp_parse_args( (array) $instance, $defaults );
+
+		/* Create an array of archive types. */
+		$type = array( 
+			'tabs'   => esc_attr__( 'Tabs',   'tabs-and-jazz' ), 
+			'toggle' => esc_attr__( 'Toggle', 'tabs-and-jazz' ), 
+		);
+
+		/* Create an array of order options. */
+		$order = array(
+			'ASC'  => esc_attr__( 'Ascending', 'tabs-and-jazz' ),
+			'DESC' => esc_attr__( 'Descending', 'tabs-and-jazz' )
+		);
+
+		/* Create an array of orderby options. */
+		$orderby = array( 
+			'author' => esc_attr__( 'Author', 'tabs-and-jazz' ),
+			'date'   => esc_attr__( 'Date', 'tabs-and-jazz' ),
+			'ID'     => esc_attr__( 'ID', 'tabs-and-jazz' ),  
+			'rand'   => esc_attr__( 'Random', 'tabs-and-jazz' ),
+			'name'   => esc_attr__( 'Slug', 'tabs-and-jazz' ),
+			'title'  => esc_attr__( 'Title', 'tabs-and-jazz' ),
+		);
+		?>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'tabs-and-jazz' ); ?></label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $instance['title'] ); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'type' ); ?>"><code>type</code></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id( 'type' ); ?>" name="<?php echo $this->get_field_name( 'type' ); ?>">
+				<?php foreach ( $type as $option_value => $option_label ) { ?>
+					<option value="<?php echo esc_attr( $option_value ); ?>" <?php selected( $instance['type'], $option_value ); ?>><?php echo esc_html( $option_label ); ?></option>
+				<?php } ?>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'group' ); ?>"><code>group</code></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id( 'group' ); ?>" name="<?php echo $this->get_field_name( 'group' ); ?>">
+				<?php foreach ( $terms as $term ) { ?>
+					<option value="<?php echo esc_attr( $term->slug ); ?>" <?php selected( $instance['group'], $term->slug ); ?>><?php echo esc_html( $term->name ); ?></option>
+				<?php } ?>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'limit' ); ?>"><code>limit</code></label>
+			<input type="text" class="code" id="<?php echo $this->get_field_id( 'limit' ); ?>" name="<?php echo $this->get_field_name( 'limit' ); ?>" value="<?php echo esc_attr( $instance['limit'] ); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'order' ); ?>"><code>order</code></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id( 'order' ); ?>" name="<?php echo $this->get_field_name( 'order' ); ?>">
+				<?php foreach ( $order as $option_value => $option_label ) { ?>
+					<option value="<?php echo esc_attr( $option_value ); ?>" <?php selected( $instance['order'], $option_value ); ?>><?php echo esc_html( $option_label ); ?></option>
+				<?php } ?>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'orderby' ); ?>"><code>orderby</code></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id( 'orderby' ); ?>" name="<?php echo $this->get_field_name( 'orderby' ); ?>">
+				<?php foreach ( $orderby as $option_value => $option_label ) { ?>
+					<option value="<?php echo esc_attr( $option_value ); ?>" <?php selected( $instance['orderby'], $option_value ); ?>><?php echo esc_html( $option_label ); ?></option>
+				<?php } ?>
+			</select>
+		</p>
+	<?php
+	}
 }
 
 ?>
--- includes/widgets.php
+++ includes/widgets.php
@@ -4,8 +4,8 @@ add_action( 'widgets_init', 'taj_register_widgets' );;
 
 function taj_register_widgets() {
 
-	// require_once( TAJ_DIR . 'class-tabs-widget.php' );
-	// register_widget( 'TAJ_Tabs_Widget' );
+	require_once( TAJ_DIR . 'includes/class-tabs-widget.php' );
+	register_widget( 'TAJ_Widget_Tabs' );
 }
 
 ?>
