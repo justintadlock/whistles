f8a89313e619b47b0b7977c575106de71975e8f3
Justin Tadlock <justin@justintadlock.com>
2013-09-08 20:31:49 -0500

    functions.php cleanup and more inline docs.

--- inc/functions.php
+++ inc/functions.php
@@ -10,6 +10,49 @@ add_action( 'init', 'whistles_register_shortcodes' );
 add_action( 'widgets_init', 'whistles_register_widgets' );
 
 /**
+ * Wrapper function for outputting whistles.  You can call one of the classes directly, but it's best to use 
+ * this function if needed within a theme template.
+ *
+ * @since  0.1.0
+ * @access public
+ * @return string
+ */
+function whistles_get_whistles( $args = array() ) {
+
+	/* Allow types other than 'tabs' or 'toggle'. */
+	$allowed = apply_filters( 'whistles_allowed_types', array( 'tabs', 'toggle' ) );
+
+	/* Clean up the type and allow typos of 'tabs' and 'toggle'. */
+	$args['type'] = sanitize_key( strtolower( $args['type'] ) );
+
+	if ( 'tab' === $args['type'] )
+		$args['type'] = 'tabs';
+
+	elseif ( 'toggles' === $args['type'] )
+		$args['type'] = 'toggle';
+
+	/* ================================== */
+
+	/* Only allow a 'type' from the $allowed_types array. */
+	$type = $args['type'] = ( isset( $args['type'] ) && in_array( $args['type'], $allowed ) ) ? $args['type'] : 'tabs';
+
+	/**
+	 * Developers can overwrite the whistles object at this point.  This is basically to bypass the 
+	 * plugin's classes and use your own.  You must return an object, not a class name.  This object 
+	 * must also have a method named "get_markup()" for returning the HTML markup.  It's best to simply 
+	 * extend Whistles_And_Bells and follow the structure outlined in that class.
+	 */
+	$whistles_object = apply_filters( 'whistles_object', null, $args );
+
+	/* If no object was returned, use one of the plugin's defaults. */
+	if ( !is_object( $whistles_object ) )
+		$whistles_object = $type === 'toggle' ? new Whistles_And_Toggles( $args ) : new Whistles_And_Tabs( $args );
+
+	/* Return the HTML markup. */
+	return $whistles_object->get_markup();
+}
+
+/**
  * Registers the [whistles] shortcode.
  *
  * @since  0.1.0
@@ -32,30 +75,6 @@ function whistles_register_widgets() {
 	require_once( WHISTLES_DIR . 'inc/class-whistles-widget.php' );
 
 	register_widget( 'WHISTLES_WIDGET' );
-}
-
-/**
- * Wrapper function for outputting whistles.  You can call one of the classes directly, but it's best to use 
- * this function if needed within a theme template.
- *
- * @since  0.1.0
- * @access public
- * @return string
- */
-function whistles_get_whistles( $args = array() ) {
-
-	$allowed = apply_filters( 'whistles_allowed_types', array( 'tabs', 'toggle' ) );
-
-	$type = $args['type'] = ( isset( $args['type'] ) && in_array( $args['type'], $allowed ) ) ? $args['type'] : 'tabs';
-
-	$whistles_object = apply_filters( 'whistles_object', null, $args );
-
-	unset( $args['type'] );
-
-	if ( !is_object( $whistles_object ) )
-		$whistles_object = $type === 'toggle' ? new Whistles_And_Toggles( $args ) : new Whistles_And_Tabs( $args );
-
-	return $whistles_object->get_markup();
 }
 
 /**
